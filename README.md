515e9ed9af24d0e4b5abdebe0f937dc7d58b5ec4
Philip Birk-Jensen <philip@b14.dk>
2015-07-31 11:16:21 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # jQuery-floatlabel
 Demo: http://codepen.io/BirkAndMe/pen/gbVEKL/
+
+## Usage
+Simply call the floatlabel() function on a jQuery element
+```javascript
+$(function () {
+  $('.form-element').floatlabel()
+});
+```
+
+Now you can use the 3 floatlabel classes to style the elements:
+```css
+.form-element {
+  /* The normal state. */
+}
+.form-element.focus {
+  /* The element is in focus. */
+}
+.form-element.has-content {
+  /* The element has content. */
+}
+.form-element.active {
+  /* The element is active, which means it's either in focus, or it has content. */
+}
+```
+The ```.active``` class is just a quick way of combining the ```.focus``` and ```.has-content``` class into one.
+
+### Handling changes in JavaScript
+An event will be triggered when a floatlabel element changes state.
+
+```javascript
+$(function () {
+  $('.form-element').floatlabel()
+    .on('floatlabel-change', function (evt, states) {
+      /* Do whatever here, depending on the states. */
+    });
+});
+```
--- jquery.floatlabel.js
+++ jquery.floatlabel.js
@@ -1,3 +1,4 @@
+/*jshint jquery: true, forin:false, browser:true, indent:2, trailing:true, unused:false */
 (function(scope, $) {
   'use strict';
 
@@ -10,6 +11,8 @@
       has_content: 'has-content',
     }, options);
 
+    // A wrapper function, that triggers that specific elements "float-change"
+    // event.
     function triggerChange($element) {
       $element.trigger('floatlabel-change', {
         focus: $element.hasClass(options.prefix + options.focus),
@@ -18,22 +21,29 @@
       });
     }
 
-    // Run through the elements.
+    // Iterate through the given elements.
     return this.each(function() {
       var
       	$container = $(this),
         $input = $('input', this);
 
+      // Bail out if there's no $input.
+      if ($input.length === 0) {
+        return;
+      }
+
+      // Setup all the events on the input field.
       $input
+        // Always add the FOCUS and ACTIVE class when the element is in focus.
         .bind('focus.floatlabel', function () {
-          // We always want the focus and active class on when the element is
-          // in focus.
           $container
             .addClass(options.prefix + options.focus)
             .addClass(options.prefix + options.active);
           triggerChange($container);
         })
 
+        // On blur remove the FOCUS class no matter what, but keep the ACTIVE
+        // class if there's any content.
         .bind('blur.floatlabel', function () {
           if (!hasContent($input)) {
             $container.removeClass(options.prefix + options.active);
@@ -43,6 +53,8 @@
           triggerChange($container);
       	})
 
+        // Update the HAS_CONTENT class depending on if there's any content in
+        // the element.
       	.bind('change.floatlabel keyup.floatlabel blur.floatlabel keydown.floatlabel', function () {
         	if (hasContent($input)) {
             if (!$container.hasClass(options.prefix + options.has_content)) {
@@ -56,10 +68,14 @@
             }
           }
       	})
+
+        // By triggering the blur event, it will set the correct classes when
+        // the function is run.
         .triggerHandler('blur.floatlabel');
     });
   };
 
+  // Private static function, that tells us if the element has any content.
   function hasContent($element) {
     return $element.val() !== '';
   }
